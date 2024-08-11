dbca1a16af840db4e0423876f491085eb88b6854
Woojun-Yoon <dbsdnwns11@naver.com>
2024-08-12 04:02:54 +0900

    docs: Update Project Page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Advirtual',
+    description: `Advirtual은 증강 현실 기술을 누구나 쉽고 빠르게 활용할 수 있도록 도와주는 서비스입니다. Spring Boot를 통한 Backend와 Infra, DevOps 등 다양한 역할을 맡고있습니다.`,
+    imgSrc: '/static/images/advirtual-logo.png',
+    href: 'https://advirtual.com',
   },
 ]
 
