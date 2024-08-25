357bcede37c694b1281337ea2c24d6ab68589944
Woojun-Yoon <dbsdnwns11@naver.com>
2024-08-25 15:06:46 +0900

    refactor: 오타 수정

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,8 +7,8 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Advirtual',
-    description: `Advirtual은 증강 현실 기술을 누구나 쉽고 빠르게 활용할 수 있도록 도와주는 서비스입니다. Spring Boot를 통한 Backend와 Infra, DevOps 등 다양한 역할을 맡고있습니다.`,
+    title: 'AdVirtual',
+    description: `AdVirtual은 증강 현실 기술을 누구나 쉽고 빠르게 활용할 수 있도록 도와주는 서비스입니다. Spring Boot를 통한 Backend와 Infra, DevOps 등 다양한 역할을 맡고있습니다.`,
     imgSrc: '/static/images/advirtual-logo.png',
     href: 'https://advirtual.com',
   },
